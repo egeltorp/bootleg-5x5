2a50aefa1ed40aa1d724a82b20d7bd0b0204fa87
egeltorp <35803316+egeltorp@users.noreply.github.com>
2025-10-05 04:57:36 +0200

    re-fix wrong fixes

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,9 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-export default defineConfig({
-  base: "/bootleg-5x5/", // 👈 REPO NAME HERE
+// https://vitejs.dev/config/
+export default defineConfig(({ command }) => ({
+  base: "/",
   server: {
     host: "::",
     port: 8080,
@@ -14,4 +15,4 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-});
+}));
